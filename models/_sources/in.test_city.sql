afa8c82934bc3a4f47cb4268663e4b4fba4de15e
Jiří Novák <jiri.novak@keboola.com>
2024-09-30 14:13:03 +0200

    Test cities

---
+++ models/_sources/in.test_city.sql
@@ -0,0 +1,22 @@
+{{ config(
+    materialized='table'
+) }}
+
+WITH test_city AS (
+    SELECT
+        NULL AS id,
+        'Prague' AS city,
+        1300000 AS population
+    UNION ALL
+    SELECT
+        2 AS id,
+        'Brno' AS city,
+        380000 AS population
+    UNION ALL
+    SELECT
+        3 AS id,
+        'Ostrava' AS city,
+        290000 AS population
+)
+
+SELECT * FROM test_city
