76b3dc92843e627a67244afd2bd48e18fcb360ce
Jiří Novák <jiri.novak@keboola.com>
2024-09-30 14:25:58 +0200

    Test cities

--- tests/assert_monthly_revenue_not_null.sql
+++
@@ -1,7 +0,0 @@
-{{ config(severity = 'warn') }}
-
-select 
-    "mql_id"
-    ,"declared_monthly_revenue"::decimal(15,4)
-from {{ source('in.c-data_import', 'olist_closed_deals_dataset') }}
-having not("declared_monthly_revenue">=0)
